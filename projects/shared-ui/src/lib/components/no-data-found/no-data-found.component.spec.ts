82aa93e9bdf0dbd8ce1682183dcd5fa998b48eba
Luis <lmejias@telynet.com>
2021-10-17 11:10:19 -0300

    add unit-test for no-data-found components

--- projects/shared-ui/src/lib/components/no-data-found/no-data-found.component.spec.ts
+++ projects/shared-ui/src/lib/components/no-data-found/no-data-found.component.spec.ts
@@ -1,22 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { NoDataFoundComponent } from './no-data-found.component';
 
 describe('NoDataFoundComponent', () => {
   let component: NoDataFoundComponent;
-  let fixture: ComponentFixture<NoDataFoundComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NoDataFoundComponent ]
-    })
-    .compileComponents();
-  });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NoDataFoundComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = new NoDataFoundComponent();
   });
 
   it('should create', () => {
--- projects/shared-ui/src/lib/components/no-data-found/no-data-found.component.ts
+++ projects/shared-ui/src/lib/components/no-data-found/no-data-found.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'su-no-data-found',
   templateUrl: './no-data-found.component.html',
   styleUrls: ['./no-data-found.component.scss']
 })
-export class NoDataFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NoDataFoundComponent {}
